54daa697c3ecc6997a05ca9bd798800bbc32136c
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-11 14:51:31 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,12 @@
 class User
 
-attr_accessor :name
+attr_accessor :first_name, :last_name
 
-def initialize
-  @name = name
+def initialize(first_name, last_name)
+  @first_name = first_name
+  @last_name = last_name
 end
 
-def first_name
   
 
 end
